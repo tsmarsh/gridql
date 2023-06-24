f9f1f57d150c3f40dd57bd498217cac3af31e2ef
Tom Marsh <ts.marsh@gmail.com>
2023-06-24 06:12:13 -0400

    Not sure how the tests were passing

--- packages/mongo-connector/index.js
+++ packages/mongo-connector/index.js
@@ -1,27 +1,27 @@
-const promiseRetry = require('promise-retry');
-const {MongoClient} = require('mongodb');
+const promiseRetry = require("promise-retry");
+const { MongoClient } = require("mongodb");
 
 const options = {
-    directConnection: true,
+  directConnection: true,
 };
 
 const promiseRetryOptions = {
-    retries: 10,
-    factor: 1.5,
-    minTimeout: 1000,
-    maxTimeout: 5000,
+  retries: 10,
+  factor: 1.5,
+  minTimeout: 1000,
+  maxTimeout: 5000,
 };
 
 async function buildDb(mongo) {
-    let client = new MongoClient(mongo["uri"], options);
-    promiseRetry((retry, number) => {
-        console.log(`MongoClient connecting to ${url} - retry number: ${number}`);
-        return client.connect().catch(retry);
-    }, promiseRetryOptions);
+  let client = new MongoClient(mongo["uri"], options);
+  promiseRetry((retry, number) => {
+    console.log(`MongoClient connecting to ${uri} - retry number: ${number}`);
+    return client.connect().catch(retry);
+  }, promiseRetryOptions);
 
-    return client.db(mongo["db"]).collection(mongo["collection"]);
+  return client.db(mongo["db"]).collection(mongo["collection"]);
 }
 
 module.exports = {
-    buildDb
-}
+  buildDb,
+};
