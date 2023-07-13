a304e3d988f5c18e2a7470f0f56fb6feb4377266
Tom Marsh <ts.marsh@gmail.com>
2023-07-13 11:22:27 -0400

    Testing sad path

--- packages/payload-validator/test/index.spec.js
+++ packages/payload-validator/test/index.spec.js
@@ -1,50 +1,85 @@
-const {valid} = require("../index.js");
-const {builderFactory} = require("@gridql/payload-generator");
+const { valid } = require("../index.js");
+const { builderFactory } = require("@gridql/payload-generator");
 const assert = require("assert");
 
+describe("test a payload against a schema", function () {
+  it("creates a validation function from a json schema", function () {
+    const schema = {
+      type: "object",
+      properties: {
+        object_id: {
+          type: "string",
+          format: "uuid",
+        },
+        firstName: {
+          type: "string",
+          faker: "name.firstName",
+        },
+        lastName: {
+          type: "string",
+          faker: "name.lastName",
+        },
+        displayName: {
+          type: "string",
+          faker: "animal.horse",
+        },
+        email: {
+          type: "string",
+          format: "email",
+          faker: "internet.email",
+        },
+        notificationEmail: {
+          type: "string",
+          format: "email",
+          faker: "internet.email",
+        },
+      },
+      required: ["firstName", "lastName", "email"],
+    };
 
-describe("test a payload against a schema", function() {
-   it("creates a validation function from a json schema", function() {
-         const schema = {
-             "type": "object",
-             "properties": {
-                 "object_id": {
-                     "type": "string",
-                     "format": "uuid"
-                 },
-                 "firstName": {
-                     "type": "string",
-                     "faker": "name.firstName"
-                 },
-                 "lastName": {
-                     "type": "string",
-                     "faker": "name.lastName"
-                 },
-                 "displayName": {
-                     "type": "string",
-                     "faker": "animal.horse"
-                 },
-                 "email": {
-                     "type": "string",
-                     "format": "email",
-                     "faker": "internet.email"
-                 },
-                 "notificationEmail": {
-                     "type": "string",
-                     "format": "email",
-                     "faker": "internet.email"
-                 }
-             },
-             "required": [
-                 "firstName",
-                 "lastName",
-                 "email"
-             ]
-         }
+    const validate = valid(schema);
+    const payload = builderFactory(schema)();
+    const result = validate(payload);
+    assert(result);
+  });
 
-         const validate = valid(schema);
-         const payload = builderFactory(schema)();
-         const result = validate(payload);
-         assert(result);
-   });
+  it("the validation function fails if the doc isn't valid", function () {
+    const schema = {
+      type: "object",
+      properties: {
+        object_id: {
+          type: "string",
+          format: "uuid",
+        },
+        firstName: {
+          type: "string",
+          faker: "name.firstName",
+        },
+        lastName: {
+          type: "string",
+          faker: "name.lastName",
+        },
+        displayName: {
+          type: "string",
+          faker: "animal.horse",
+        },
+        email: {
+          type: "string",
+          format: "email",
+          faker: "internet.email",
+        },
+        notificationEmail: {
+          type: "string",
+          format: "email",
+          faker: "internet.email",
+        },
+      },
+      required: ["firstName", "lastName", "email"],
+    };
+
+    const validate = valid(schema);
+    const payload = {};
+    const result = validate(payload);
+    assert(!result);
+  });
 });
