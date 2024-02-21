03742f12303b1816fac834ec0b7eed1a3d9aca84
Tom Marsh <ts.marsh@gmail.com>
2024-02-21 10:28:54 -0500

    First pass of unit test complete

--- packages/blocks/test/GraphCall.spec.js
+++ packages/blocks/test/GraphCall.spec.js
@@ -4,25 +4,25 @@ const fetchMock = require("fetch-mock");
 const {GotIt} = require("./GotIt");
 
 describe("Should create a query from a template and forward the results", async function () {
+    const data = {
+        farm_id: "diddly squat",
+        time: 12952932
+    }
+
+    const template = '{\n' +
+        '         getById(id: "${farm_id}", at: ${time}) {\n' +
+        '               name \n' +
+        '               coops {\n' +
+        '                name\n' +
+        '                hens {\n' +
+        '                  eggs\n' +
+        '                  name\n' +
+        '                }\n' +
+        '               }\n' +
+        '            }\n' +
+        '        }'
+
     it("should fill in a query template", async () => {
-        const template = '{\n' +
-            '         getById(id: "${farm_id}", at: ${time}) {\n' +
-            '               name \n' +
-            '               coops {\n' +
-            '                name\n' +
-            '                hens {\n' +
-            '                  eggs\n' +
-            '                  name\n' +
-            '                }\n' +
-            '               }\n' +
-            '            }\n' +
-            '        }'
-
-        const data = {
-            farm_id: "diddly squat",
-            time: 12952932
-        }
-
         const gc = new GraphCall(null, null, template, null);
 
         const result = gc.fillTemplate(data);
@@ -63,6 +63,32 @@ describe("Should create a query from a template and forward the results", async 
         const gc = new GraphCall("http://testgraph.test", null, null, {"servererror": g});
 
         const result = await gc.callServer(body);
+        assert(g.called);
+        fetchMock.reset();
+    })
+
+    it("should parse out the data and return the json", async ()=> {
+        fetchMock.post("http://testgraph.test", {data: {"getByName": {"name": "diddly squat"}}});
+
+        const g = new GotIt({"name": "diddly squat"});
+
+        const gc = new GraphCall("http://testgraph.test", "getByName", template, {"success": g});
+
+        await gc.execute(data);
+
+        assert(g.called);
+        fetchMock.reset();
+    })
+
+    it("should pass to error if the response isn't json", async ()=> {
+        fetchMock.post("http://testgraph.test", "definitely not json");
+
+        const g = new GotIt(data);
+
+        const gc = new GraphCall("http://testgraph.test", "getByName", template, {"error": g});
+
+        await gc.execute(data);
+
         assert(g.called);
         fetchMock.reset();
     })
