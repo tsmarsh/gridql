1f2dbb7a89747d5db35a3bab4160db042b81df0a
Tom Marsh <ts.marsh@gmail.com>
2024-03-13 20:55:26 -0400

    tests pass again

--- docker/gridql/index.js
+++ docker/gridql/index.js
@@ -1,15 +1,17 @@
-const {parse, build_app} = require("@gridql/server")
-const fs = require("fs")
+import {build_app} from "@gridql/server";
+
+import {parse} from "@gridql/server/lib/config.js";
+import fs from "fs";
 
 let configPath = "./config/config.conf";
-if(fs.existsSync(configPath)){
-    parse(configPath)
-        .then(config => {
-            console.log("Graphlettes: ", config.graphlettes.length)
-            console.log("Restlettes: ", config.restlettes.length)
-            build_app(config).then(app => app.listen(config.port))
-        }).catch(err => console.log("Error parsing config: ", err))
-}else{
-    console.log("Config missing");
+if (fs.existsSync(configPath)) {
+  parse(configPath)
+    .then((config) => {
+      console.log("Graphlettes: ", config.graphlettes.length);
+      console.log("Restlettes: ", config.restlettes.length);
+      build_app(config).then((app) => app.listen(config.port));
+    })
+    .catch((err) => console.log("Error parsing config: ", err));
+} else {
+  console.log("Config missing");
 }
-
--- docker/gridql/test/docker.spec.js
+++ docker/gridql/test/docker.spec.js
@@ -1,44 +1,52 @@
-const {OpenAPIClientAxios} = require("openapi-client-axios");
-const {builderFactory} = require("@gridql/payload-generator")
-const assert = require("assert");
+import { OpenAPIClientAxios } from "openapi-client-axios";
 
-const fs = require("fs");
-const {DockerComposeEnvironment} = require("testcontainers");
+import { builderFactory } from "@gridql/payload-generator";
+
+import assert from "assert";
+
+import fs from "fs";
+
+import { DockerComposeEnvironment } from "testcontainers";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import { before, describe, it } from "mocha";
 
 let swagger_clients = {};
 
 let schema;
 
-let environment;
-
 before(async function () {
-    this.timeout(200000);
+  this.timeout(200000);
 
-    environment = await new DockerComposeEnvironment(__dirname , "gridql-test-compose.yml")
-        .up();
+  await new DockerComposeEnvironment(__dirname, "gridql-test-compose.yml").up();
 
-    for(let restlette of ["test"]){
-        let rest = await fetch(`http://localhost:3033/${restlette}/api/api-docs/swagger.json`)
-        let swaggerdoc = await rest.json();
-        let api = new OpenAPIClientAxios({ definition: swaggerdoc });
-        swagger_clients[`/${restlette}/api`] = await api.init()
-    }
+  for (let restlette of ["test"]) {
+    let rest = await fetch(
+      `http://localhost:3033/${restlette}/api/api-docs/swagger.json`,
+    );
+    let swaggerdoc = await rest.json();
+    let api = new OpenAPIClientAxios({ definition: swaggerdoc });
+    swagger_clients[`/${restlette}/api`] = await api.init();
+  }
 
-    schema = JSON.parse(fs.readFileSync(__dirname + "/config/json/test.schema.json").toString());
-
+  schema = JSON.parse(
+    fs.readFileSync(__dirname + "/config/json/test.schema.json").toString(),
+  );
 });
 
 describe("Should build docker image and run", function () {
-    it("should create a test", async () => {
+  it("should create a test", async () => {
+    let test_factory = builderFactory(schema);
+    let test = test_factory();
 
-        let test_factory = builderFactory(schema)
-        test = test_factory()
+    const result = await swagger_clients["/test/api"].create(null, test);
 
-        const result = await swagger_clients["/test/api"].create(null, test);
-
-        assert.equal(result.status, 200);
-        assert.equal(result.data.name, test.name);
-        assert(result.data.id !== undefined);
-    })
+    assert.equal(result.status, 200);
+    assert.equal(result.data.name, test.name);
+    assert(result.data.id !== undefined);
+  });
 });
-
