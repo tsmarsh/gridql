98de69564d97b4089855558650284bb1c1111ab2
Tom Marsh <ts.marsh@gmail.com>
2024-03-21 16:27:07 -0400

    fixing payload generator

--- packages/payload-generator/index.js
+++ packages/payload-generator/index.js
@@ -1,21 +1,21 @@
-import jsf from "json-schema-faker";
+import {JSONSchemaFaker} from "json-schema-faker";
 
 import { faker } from "@faker-js/faker";
 
 export function builderFactory(schema) {
-  jsf.extend("faker", () => faker);
-  jsf.format("date", () => faker.date.past().toISOString().split("T")[0]);
-  jsf.format("id", () => faker.datatype.uuid());
+  JSONSchemaFaker.extend("faker", () => faker);
+  JSONSchemaFaker.format("date", () => faker.date.past().toISOString().split("T")[0]);
+  JSONSchemaFaker.format("id", () => faker.string.uuid());
 
   return (n) => {
     if (n !== undefined) {
       const results = [];
       for (let i = 0; i < n; i++) {
-        results.push(jsf.generate(schema));
+        results.push(JSONSchemaFaker.generate(schema));
       }
       return results;
     } else {
-      return jsf.generate(schema);
+      return JSONSchemaFaker.generate(schema);
     }
   };
 }
